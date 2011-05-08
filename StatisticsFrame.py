27b03855f4824fca5f8396e8bd4c709e4bc19ad2
Antigluk <antigluk@gmail.com>
2011-05-08 18:29:50 +0300

    параметры фрейма статистики

--- StatisticsFrame.py
+++ StatisticsFrame.py
@@ -4,4 +4,17 @@
 import wx
 
 class StatisticsFrame(wx.Frame):
-    pass
+    def __init__(self, parent):
+        wx.Frame.__init__(
+            self,
+            parent,
+            -1,
+            'Статистика',
+            style=wx.BORDER_DEFAULT,
+            size=(320, 220),
+            )
+        self.Centre()
+
+    def update_ui(self):
+        #TODO: проверять видимо ли окно. иначе не обновлять
+        pass
