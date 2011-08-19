2e1742e8feca24514059bf651f5c8e74e8a33b87
Roman Rader <antigluk@gmail.com>
2011-08-19 11:23:10 +0300

    macos shell script for running Pomodoro

---
+++ Pomodoro-MacOS.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Pomodoro-MacOS.sh
+# Pomodoro
+#
+# Created by Roman Rader on 19.08.11.
+# New BSD License 2011 Antigluk https://github.com/antigluk/Pomodoro
+
+
+VERSIONER_PYTHON_PREFER_32_BIT=yes python pomodoro.py
